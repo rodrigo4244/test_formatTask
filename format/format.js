20ca97ad768e7a9ea56bcfe3c73e0ae6b3a7577a
digo4244 <rodrigo.4244@hotmail.com>
2017-06-29 16:10:42 -0300

    Create test file

---
+++ format/format.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const {
+  compact,
+  flow,
+  get,
+  join,
+} = require('lodash/fp')
+
+/**
+ * Returns a formatted link to a channel for use with the Slack API
+ * @method channelLink
+ * @param {String} slackChannelId - format is like C024BE7LR
+ * @param {String} display - optional readable name
+ * @return {String} - The link to the channel formatted for Slack API
+ */
+function channelLink(slackChannelId, display) {
+  if (display) return `<#${slackChannelId}|${display}>`
+  return `<#${slackChannelId}>`
+}
+
+function userLinkDisplay(user) {
+  if (get('username')(user)) return `@${user.username}`
+  if (user) return `@${user}`
+  return 'None'
+}
+
+function formatTask(task) {
+  const cleanJoin = flow(compact, join(' '))
+  return cleanJoin([
+    task.isCompleted() ? '' : ':warning: ',
+    task.slackChannelId ? channelLink(task.slackChannelId) : '',
+    task.name,
+  ])
+}
+
+module.exports = {
+  formatTask,
+  userLinkDisplay,
+}
---
+++ format/format.spec.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const assert = require('assert')
+
+const {
+  userLinkDisplay,
+} = require('format.js')
+
+describe('format', () => {
+  describe('userLinkDisplay', () => {
+    it('should generate the user link for display', () => {
+      const username = 'rodrigo'
+      const user = { username }
+
+      const userLink = userLinkDisplay(user)
+
+      assert.strictEqual(`@${username}`, userLink)
+    })
+
+    it('should return None when user doesn\'t exist', () => {
+      const userLink = userLinkDisplay()
+
+      assert.strictEqual('None', userLink)
+    })
+  })
+
+  describe('formatTask')
+})
