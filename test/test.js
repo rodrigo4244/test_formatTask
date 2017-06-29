32f9ce0b5fae68e8db4feafc9c0509ba6f61b12d
digo4244 <rodrigo.4244@hotmail.com>
2017-06-29 20:31:48 -0300

    Write tests (incomplete)

--- test/test.js
+++ test/test.js
@@ -7,51 +7,71 @@ const {
   formatTask,
 } = require('../format/format.js')
 
-describe('formatTask', () => {
-    it('Should return \'<#C024BE7LR> taskOne\' for this set of parameters', () => {
-        const task = {
-            isCompleted: function() {return true},
-            slackChannelId: 'C024BE7LR',
-            name: 'taskOne',
-        }
-        const output = formatTask(task);
+describe('format', () => {
+  describe('userLinkDisplay', () => {
+    it('should generate the user link for display', () => {
+      const username = 'rodrigo'
+      const user = { username }
 
-        assert.strictEqual(`<#${task.slackChannelId}> ` + `${task.name}`, output)
+      const userLink = userLinkDisplay(user)
+
+      assert.strictEqual(`@${username}`, userLink)
+    })
+
+    it('should return None when user doesn\'t exist', () => {
+      const userLink = userLinkDisplay()
+
+      assert.strictEqual('None', userLink)
+    })
+  })
+
+  describe('formatTask', () => {
+    it('Should return \'<#C024BE7LR> taskOne\' for this set of parameters', () => {
+      const task = {
+        isCompleted() { return true },
+        slackChannelId: 'C024BE7LR',
+        name: 'taskOne',
+      }
+      const output = formatTask(task)
+
+      assert.strictEqual(`<#${task.slackChannelId}> ` + `${task.name}`, output)
     })
 
     it('Should return \':warning:  <#C024BE7LR> taskOne\' for this set of parameters', () => {
-        const task = {
-            isCompleted: function() {return false},
-            slackChannelId: 'C024BE7LR',
-            name: 'taskOne',
-        }
-        const output = formatTask(task)
+      const task = {
+        isCompleted() { return false },
+        slackChannelId: 'C024BE7LR',
+        name: 'taskOne',
+      }
+      const output = formatTask(task)
 
-        assert.strictEqual(':warning:  ' + `<#${task.slackChannelId}> ` + `${task.name}`, output) //there's a space after :warning: and after <#${task.slackChannelId}>
+      assert.strictEqual(':warning:  ' + `<#${task.slackChannelId}> ` + `${task.name}`, output) // there's a space after :warning: and after <#${task.slackChannelId}>
     })
 
     it('Should return \':warning:  taskOne\' for this set of parameters', () => {
-        const task = {
-            isCompleted: function() {return false},
-            slackChannelId: false,
-            name: 'taskOne',
-        }
-        const output = formatTask(task)
+      const task = {
+        isCompleted() { return false },
+        slackChannelId: false,
+        name: 'taskOne',
+      }
+      const output = formatTask(task)
 
-        assert.strictEqual(':warning: ' + ' ' + `${task.name}`, output)
+      assert.strictEqual(':warning: ' + ' ' + `${task.name}`, output) // this ' ' is because of the join inside format.js
     })
 
     it('Should return \'taskOne\' for this set of parameters', () => {
-        const task = {
-            isCompleted: function() {return true},
-            slackChannelId: false,
-            name: 'taskOne',
-        }
-        const output = formatTask(task)
+      const task = {
+        isCompleted() { return true },
+        slackChannelId: false,
+        name: 'taskOne',
+      }
+      const output = formatTask(task)
 
-        assert.strictEqual(task.name, output)
+      assert.strictEqual(task.name, output)
     })
+  })
 })
+
 
 /*
 describe('Array', () => {
